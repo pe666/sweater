73d770f54fde8e000c608a26662115f74899c649
Alexey <lexasiv1969@gmail.com>
2020-01-06 16:02:38 +0300

    added interaction with mysql

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,24 +1,57 @@
 package com.example.sweater;
 
 
+import com.example.sweater.domain.Message;
+import com.example.sweater.repo.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class GreetingController {
-
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required = false, defaultValue = "world") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name="name", required = false, defaultValue = "world") String name, Map<String, Object> model) {
+        model.put("name", name);
 
         return "greeting";
     }
 
     @GetMapping
-    public String home(@RequestParam(name="name", required = false, defaultValue = "world") String name, Model model) {
+    public String home(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "home";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "home";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
         return "home";
     }
 }
